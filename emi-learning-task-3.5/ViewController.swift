c2a66cd4589eb3611cf315a97ab9a5a57c53e093
Luis Felipe dos Reis Santos <luis.felipe@zup.com.br>
2022-10-18 13:06:04 -0300

    finished task

--- emi-learning-task-3.5/ViewController.swift
+++ emi-learning-task-3.5/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nomeTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var areaTextField: UITextField!
+    @IBOutlet weak var statusTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func botaoReceberPressionado(_ sender: UIButton) {
+        confirmarDados()
+        enviarDados()
+    }
+    
+    func confirmarDados() {
+        let titulo = "Quase lá!"
+        let mensagem = "Antes de enviarmos, por favor, revise seus dados:\n\nNome: \(nomeTextField.text!)\nEmail: \(emailTextField.text!)\nÁrea de Atuação: \(areaTextField.text!)\nStatus Profissional: \(statusTextField.text!)"
+        
+        let alert = UIAlertController(title: titulo, message: mensagem, preferredStyle: .actionSheet)
+        
+        alert.addAction(UIAlertAction(title: "Confirmar", style: .default, handler: { _ in
+            self.enviarDados()
+        }))
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func enviarDados() {
+        let alert = UIAlertController(title: "Feito!", message: "Verifique seu email e tenha acesso ao documento.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Ok!", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true, completion: nil)
+    }
 }
-
